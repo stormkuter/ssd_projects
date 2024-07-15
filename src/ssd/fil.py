2465ac05cb49ba6e17e3b24bf74b0f26f6823163
tae-eui.kim <taeeui.k@gmail.com>
2024-07-15 14:55:41 +0900

    [refactor] data 폴더 생성

--- src/ssd/fil.py
+++ src/ssd/fil.py
@@ -1,29 +1,8 @@
 import os
-import enum
 
-INIT_VALUE = '0x00000000'
-MAX_ADDRESS = 100
-
-
-class FilePath(enum.Enum):
-    if os.environ.get('ENV', 'prod') == 'test':
-        folder_path = os.path.join(os.path.dirname(__file__), '../../tests/data')
-    else:
-        folder_path = os.path.join(os.path.dirname(__file__), 'data')
-    nand_file_path = os.path.join(folder_path, './nand.txt')
-    result_file_path = os.path.join(folder_path, './result.txt')
-
-    @classmethod
-    def get_folder_path(cls):
-        return FilePath.folder_path
-
-    @classmethod
-    def get_nand_file_path(cls):
-        return FilePath.nand_file_path
-
-    @classmethod
-    def get_result_file_path(cls):
-        return FilePath.result_file_path
+FOLDER_PATH = os.path.join(os.path.dirname(__file__), 'data')
+NAND_FILE_PATH = os.path.join(FOLDER_PATH, './nand.txt')
+INIT_VALUE = 0x00000000
 
 
 class FlashInterfaceLayer:
@@ -31,52 +10,29 @@ class FlashInterfaceLayer:
         self.__flash_map = {}
         self.__lazy_update = False
 
-        self.folder_path = FilePath.get_folder_path().value
-        self.nand_file_path = FilePath.get_nand_file_path().value
-        self.result_file_path = FilePath.get_result_file_path().value
-
         # .data/nand.txt, result.txt 에 유효한 정보가 있다면 읽어서 가져옴
-        if not os.path.exists(self.folder_path):
-            os.mkdir(self.folder_path)
+        if not os.path.exists(FOLDER_PATH):
+            os.mkdir(FOLDER_PATH)
 
-        if not os.path.exists(self.nand_file_path):
-            with open(self.nand_file_path, "w") as f:
-                for i in range(MAX_ADDRESS - 1):
-                    f.write(f'{INIT_VALUE}\n')
-                f.write(f'{INIT_VALUE}')
 
-        if not os.path.exists(self.result_file_path):
-            with open(self.result_file_path, "w") as f:
-                f.write(f'{INIT_VALUE}')
-
-        with open(self.nand_file_path, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-
-        for i, line in enumerate(lines):
-            self.__flash_map[str(i)] = line.strip()
+        pass
 
     def write_lba(self, lba, value):
         self.__flash_map[lba] = value
         if not self.__lazy_update:
-            with open(self.nand_file_path, "w") as f:
-                for i in range(MAX_ADDRESS - 1):
-                    f.write(f'{self.__flash_map[str(i)]}\n')
-                f.write(f'{self.__flash_map[str(MAX_ADDRESS - 1)]}')
+            # update nand.txt
+            # with open(filename, "w") as f:
+            #     result = f.write(value)
+            pass
+
+        # return result
 
     def read_lba(self, lba):
         if not self.__lazy_update:
             # update result.txt
-            with open(self.result_file_path, "w") as f:
-                f.write(self.__flash_map[lba])
             pass
         return self.__flash_map[lba]
 
     def flush(self):
         # update nand.txt, result.txt
         pass
-
-    def enable_lasy_update(self):
-        self.__lazy_update = False
-
-    def disable_lasy_update(self):
-        self.__lazy_update = True
--- tests/test_ssd_fil.py
+++ tests/test_ssd_fil.py
@@ -1,67 +1,24 @@
 import unittest
 import pathlib as pl
-import shutil
 import os
-from unittest import skip
 
-os.environ['ENV'] = 'test'
-from src.ssd.fil import FlashInterfaceLayer, FilePath
+from src.ssd.fil import FlashInterfaceLayer
 
-INIT_VALUE = '0x00000000'
-TEST_VALUE = '0x12345678'
-TEST_ADDRESS = '1'
-MAX_ADDRESS = 100
+FOLDER_PATH = '../src/ssd/data'
+NAND_FILE_PATH = os.path.join(FOLDER_PATH, './nand.txt')
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         super().setUp()
-
-        shutil.rmtree(os.path.join(os.path.dirname(__file__), 'data'), ignore_errors=True)
-
         self.sut = FlashInterfaceLayer()
-        self.folder_path = FilePath.get_folder_path().value
-        self.nand_file_path = FilePath.get_nand_file_path().value
-        self.result_file_path = FilePath.get_result_file_path().value
 
     def test_folder_create(self):
-        self.check_folder_exist(self.folder_path)
+        path = pl.Path(FOLDER_PATH)
 
-    def test_nand_file_create_init(self):
-        self.check_file_exist(self.nand_file_path)
-        read_data = self.get_file_data(self.nand_file_path)
-        self.assertEqual(len(read_data), MAX_ADDRESS)
-
-        for i in range(MAX_ADDRESS):
-            if read_data[i] != INIT_VALUE:
-                raise AssertionError("initial value is not matched")
-
-    def test_result_file_create_init(self):
-        read_data = self.get_file_data(self.result_file_path)
-
-        self.check_file_exist(self.result_file_path)
-        self.assertEqual(read_data[0], INIT_VALUE)
-
-    def test_write_lba_success(self):
-        self.sut.enable_lasy_update()
-        self.sut.write_lba(TEST_ADDRESS, TEST_VALUE)
-
-        self.assertEqual(self.sut.read_lba(TEST_ADDRESS), TEST_VALUE)
-
-    def test_read_lba_success(self):
-        read_data = self.get_file_data(self.result_file_path)
-        self.assertEqual(self.sut.read_lba(TEST_ADDRESS), read_data[0])
-
-    def get_file_data(self, path):
-        return pl.Path(path).read_text().split('\n')
-
-    def check_file_exist(self, path):
-        if not pl.Path(path).resolve().is_file():
-            raise AssertionError("File does not exist: %s" % str(path))
-
-    def check_folder_exist(self, path):
         if not pl.Path(path).resolve().is_dir():
             raise AssertionError("Folder does not exist: %s" % str(path))
+
 
 
 if __name__ == '__main__':
