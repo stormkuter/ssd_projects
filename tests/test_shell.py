b2b2b70b14b235260a441f2aff20402d1b959092
yoon-j <jiyoon.jeon258@gmail.com>
2024-07-15 14:40:41 +0900

    [feat] Add two test cases for testing shell write command

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,5 +1,20 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+from src.shell import Shell, WriteCommand
+
+TEST_LBA = 3
+TEST_VAL = 0xAAAABBBB
 
 
 class TestShell(TestCase):
-    pass
+    def setUp(self):
+        self.sut = Shell()
+
+    @patch.object(WriteCommand, 'execute', return_value=0)
+    def test_write_success(self, mock_write):
+        self.assertEqual(0, self.sut.write(TEST_LBA, TEST_VAL))
+
+    @patch.object(WriteCommand, 'execute', return_value=1)
+    def test_write_failure(self, mock_write):
+        self.assertEqual(1, self.sut.write(TEST_LBA, TEST_VAL))
