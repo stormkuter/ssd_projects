ca5c23510385a5295eae70959e62df43edc6ed83
tae-eui.kim <taeeui.k@gmail.com>
2024-07-15 17:31:36 +0900

    [feat] testcase 분리

--- tests/test_ssd_fil.py
+++ tests/test_ssd_fil.py
@@ -5,14 +5,23 @@ from unittest import skip
 
 from src.ssd.fil import FlashInterfaceLayer
 
-FOLDER_PATH = os.path.join(os.path.dirname(__file__), '../src/ssd/data')
+os.environ['ENV'] = 'test'
+
+FOLDER_PATH = os.path.join(os.path.dirname(__file__), 'data')
 NAND_FILE_PATH = os.path.join(FOLDER_PATH, './nand.txt')
+RESULT_FILE_PATH = os.path.join(FOLDER_PATH, './result.txt')
+# FOLDER_PATH = os.getenv('FOLDER_PATH')
+# NAND_FILE_PATH = os.getenv('NAND_FILE_PATH')
+# RESULT_FILE_PATH = os.getenv('RESULT_FILE_PATH')
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         super().setUp()
+        # print(os.environ['ENV'])
+        # print(os.path.dirname(__file__))
         self.sut = FlashInterfaceLayer()
+
 
     def test_folder_create(self):
         path = pl.Path(FOLDER_PATH)
@@ -27,6 +36,15 @@ class MyTestCase(unittest.TestCase):
             raise AssertionError("nand.txt File does not exist: %s" % str(path))
 
         self.assertEqual(len(pl.Path(path).read_text().split('\n')), 100)
+
+    @skip
+    def test_result_file_create_init(self):
+        path = pl.Path(RESULT_FILE_PATH)
+
+        if not pl.Path(path).resolve().is_file():
+            raise AssertionError("result.txt File does not exist: %s" % str(path))
+
+        self.assertEqual(len(pl.Path(path).read_text().split('\n')), 1)
 
     @skip
     def test_prev_nand_file_load(self):
