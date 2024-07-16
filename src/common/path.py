f2614a4170a0228f02e1f5ba2f941f7c9c2c5d8e
Gunho Kim <qlfydehd@gmail.com>
2024-07-16 20:53:58 +0900

    [refactor] 파일 path 문자열을 os 환경에 맞도록 정규화

--- src/common/path.py
+++ src/common/path.py
@@ -9,8 +9,8 @@ TEST_BASE_DIR = os.path.join(BASE_DIR, "tests/data")
 
 SSD_EXEC = os.path.join(SOURCE_SSD_DIR, "ssd.py")
 
-DATA_FILE_NAND = os.path.join(SOURCE_SSD_DIR, "data/nand.txt")
-DATA_FILE_RESULT = os.path.join(SOURCE_SSD_DIR, "data/result.txt")
+DATA_FILE_NAND = os.path.normpath(os.path.join(SOURCE_SSD_DIR, "data/nand.txt"))
+DATA_FILE_RESULT = os.path.normpath(os.path.join(SOURCE_SSD_DIR, "data/result.txt"))
 
 TEST_DATA_FILE_NAND = os.path.join(TEST_BASE_DIR, "nand.txt")
 TEST_DATA_FILE_RESULT = os.path.join(TEST_BASE_DIR, "result.txt")
