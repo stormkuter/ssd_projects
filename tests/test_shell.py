b3042418b7336ccad96628abe04a31c2485511c1
yj93.cho <bigeye9312@gmail.com>
2024-07-16 11:48:33 +0900

    [test] modify testcases for shell. use refactored shell operations with return (err, value) tuple.

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from src.shell.shell import Shell
+from src.shell.shell_script import ShellOperation
 from src.shell.shell_command import WriteCommand, ReadCommand, FullReadCommand, FullWriteCommand
 
 TEST_LBA = 3
@@ -12,44 +13,45 @@ TEST_VAL = 0xAAAABBBB
 
 class TestShell(TestCase):
     def setUp(self):
+        self.__op = ShellOperation()
         self.sut = Shell()
 
-    @patch.object(WriteCommand, 'execute', return_value=0)
+    @patch.object(WriteCommand, 'execute', return_value=(0, None))
     def test_write_success(self, mock_write):
-        self.assertEqual(0, self.sut.write(TEST_LBA, TEST_VAL))
+        self.assertEqual(0, self.__op.write(TEST_LBA, TEST_VAL).err)
 
-    @patch.object(WriteCommand, 'execute', return_value=1)
+    @patch.object(WriteCommand, 'execute', return_value=(1, None))
     def test_write_failure(self, mock_write):
-        self.assertEqual(1, self.sut.write(TEST_LBA, TEST_VAL))
+        self.assertEqual(1, self.__op.write(TEST_LBA, TEST_VAL).err)
 
-    @patch.object(ReadCommand, 'execute', return_value=0)
+    @patch.object(ReadCommand, 'execute', return_value=(0, None))
     def test_read_success(self, mock_read):
-        self.assertEqual(0, self.sut.read(TEST_LBA))
+        self.assertEqual(0, self.__op.read(TEST_LBA).err)
 
-    @patch.object(ReadCommand, 'execute', return_value=1)
+    @patch.object(ReadCommand, 'execute', return_value=(1, None))
     def test_read_failure(self, mock_read):
-        self.assertEqual(1, self.sut.read(TEST_LBA))
+        self.assertEqual(1, self.__op.read(TEST_LBA).err)
 
-    @patch.object(FullWriteCommand, 'execute', return_value=0)
+    @patch.object(FullWriteCommand, 'execute', return_value=(0, None))
     def test_full_write_success(self, mock_full_write):
-        self.assertEqual(0, self.sut.full_write(TEST_VAL))
+        self.assertEqual(0, self.__op.full_write(TEST_VAL).err)
 
-    @patch.object(FullWriteCommand, 'execute', return_value=1)
+    @patch.object(FullWriteCommand, 'execute', return_value=(1, None))
     def test_full_write_failure(self, mock_full_write):
-        self.assertEqual(1, self.sut.full_write(TEST_VAL))
+        self.assertEqual(1, self.__op.full_write(TEST_VAL).err)
 
-    @patch.object(FullReadCommand, 'execute', return_value=0)
+    @patch.object(FullReadCommand, 'execute', return_value=(0, None))
     def test_full_read_success(self, mock_full_read):
-        self.assertEqual(0, self.sut.full_read())
+        self.assertEqual(0, self.__op.full_read().err)
 
-    @patch.object(FullReadCommand, 'execute', return_value=1)
+    @patch.object(FullReadCommand, 'execute', return_value=(1, None))
     def test_full_read_failure(self, mock_full_read):
-        self.assertEqual(1, self.sut.full_read())
+        self.assertEqual(1, self.__op.full_read().err)
 
-    @patch.object(Shell, 'help', return_value=1)
+    @patch.object(ShellOperation, 'help', return_value=(1, None))
     def test_help(self, mock_help):
-        self.sut.help()
-        self.sut.help.assert_called_once()
+        self.__op.help()
+        self.__op.help.assert_called_once()
 
     @patch.object(Shell, '_get_user_input', return_value='exit')
     def test_run_exit_success(self, mock_write):
@@ -90,7 +92,6 @@ class TestShell(TestCase):
 
         self.assertTrue('Invalid Command' in output.getvalue())
         sys.stdout = sys.__stdout__
-
 
     @patch.object(Shell, '_get_user_input', side_effect=['fullwrite', 'exit'])
     def test_run_invalid_command_fullwrite(self, mock_write):
