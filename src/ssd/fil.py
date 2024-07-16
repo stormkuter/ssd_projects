45b19703ed81352f3826fa682772b662c3df6a78
tae-eui.kim <taeeui.k@gmail.com>
2024-07-16 09:30:59 +0900

    [refactor] file path cls method 로 분리하여 testcase에서 사용토록 수정

--- src/ssd/fil.py
+++ src/ssd/fil.py
@@ -1,7 +1,29 @@
 import os
+import enum
 
 INIT_VALUE = '0x00000000'
 MAX_ADDRESS = 100
+
+
+class FilePath(enum.Enum):
+    if os.environ['ENV'] == 'test':
+        folder_path = os.path.join(os.path.dirname(__file__), '../../tests/data')
+    else:
+        folder_path = os.path.join(os.path.dirname(__file__), 'data')
+    nand_file_path = os.path.join(folder_path, './nand.txt')
+    result_file_path = os.path.join(folder_path, './result.txt')
+
+    @classmethod
+    def get_folder_path(cls):
+        return FilePath.folder_path
+
+    @classmethod
+    def get_nand_file_path(cls):
+        return FilePath.nand_file_path
+
+    @classmethod
+    def get_result_file_path(cls):
+        return FilePath.result_file_path
 
 
 class FlashInterfaceLayer:
@@ -9,35 +31,30 @@ class FlashInterfaceLayer:
         self.__flash_map = {}
         self.__lazy_update = False
 
-        if os.environ.get('ENV', 'prod') != 'test':
-            FOLDER_PATH = os.path.join(os.path.dirname(__file__), 'data')
-            NAND_FILE_PATH = os.path.join(FOLDER_PATH, './nand.txt')
-            RESULT_FILE_PATH = os.path.join(FOLDER_PATH, './result.txt')
-        else:
-            FOLDER_PATH = os.path.join(os.path.dirname(__file__), '../../tests/data')
-            NAND_FILE_PATH = os.path.join(FOLDER_PATH, './nand.txt')
-            RESULT_FILE_PATH = os.path.join(FOLDER_PATH, './result.txt')
+        self.folder_path = FilePath.get_folder_path().value
+        self.nand_file_path = FilePath.get_nand_file_path().value
+        self.result_file_path = FilePath.get_result_file_path().value
 
         # .data/nand.txt, result.txt 에 유효한 정보가 있다면 읽어서 가져옴
-        if not os.path.exists(FOLDER_PATH):
-            os.mkdir(FOLDER_PATH)
+        if not os.path.exists(self.folder_path):
+            os.mkdir(self.folder_path)
 
-        if not os.path.exists(NAND_FILE_PATH):
-            with open(NAND_FILE_PATH, "w") as f:
+        if not os.path.exists(self.nand_file_path):
+            with open(self.nand_file_path, "w") as f:
                 for i in range(MAX_ADDRESS - 1):
-                    f.write(f'{INIT_VALUE[2:]}\n')
+                    self.__flash_map[i] = INIT_VALUE[2:]
+                    f.write(f'{self.__flash_map[i]}\n')
+                self.__flash_map[MAX_ADDRESS] = INIT_VALUE[2:]
                 f.write(f'{INIT_VALUE[2:]}')
 
-        if not os.path.exists(RESULT_FILE_PATH):
-            with open(RESULT_FILE_PATH, "w") as f:
+        if not os.path.exists(self.result_file_path):
+            with open(self.result_file_path, "w") as f:
                 f.write(f'{INIT_VALUE[2:]}')
 
     def write_lba(self, lba, value):
         self.__flash_map[lba] = value
         if not self.__lazy_update:
             pass
-
-        # return result
 
     def read_lba(self, lba):
         if not self.__lazy_update:
