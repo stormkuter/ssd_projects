2a88b8592ae8210c03750551b08b1285c613a044
tae-eui.kim <taeeui.k@gmail.com>
2024-07-17 11:21:59 +0900

    [feat] fil erase 기능 추가

--- tests/test_ssd_fil.py
+++ tests/test_ssd_fil.py
@@ -2,15 +2,14 @@ import unittest
 import pathlib as pl
 import os
 import shutil
-from unittest import skip
 
-os.environ['ENV'] = 'test'
+from src.common import ssd_config
 from src.ssd.fil import FlashInterfaceLayer
 
 INIT_VALUE = '0x00000000'
 TEST_VALUE = '0x12345678'
 TEST_ADDRESS = '1'
-MAX_ADDRESS = 100
+TEST_SIZE = '10'
 
 
 class MyTestCase(unittest.TestCase):
@@ -30,9 +29,9 @@ class MyTestCase(unittest.TestCase):
     def test_nand_file_create_init(self):
         self.check_file_exist(self.nand_file_path)
         read_data = self.get_file_data(self.nand_file_path)
-        self.assertEqual(len(read_data), MAX_ADDRESS)
+        self.assertEqual(len(read_data), ssd_config.NUM_LBAS)
 
-        for i in range(MAX_ADDRESS):
+        for i in range(ssd_config.NUM_LBAS):
             if read_data[i] != INIT_VALUE:
                 raise AssertionError("initial value is not matched")
 
@@ -43,14 +42,23 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(read_data[0], INIT_VALUE)
 
     def test_write_lba_success(self):
-        self.sut.enable_lasy_update()
+        self.sut.enable_cache()
         self.sut.write_lba(TEST_ADDRESS, TEST_VALUE)
 
         self.assertEqual(self.sut.read_lba(TEST_ADDRESS), TEST_VALUE)
 
     def test_read_lba_success(self):
-        read_data = self.get_file_data(self.result_file_path)
-        self.assertEqual(self.sut.read_lba(TEST_ADDRESS), read_data[0])
+        self.assertEqual(self.sut.read_lba(TEST_ADDRESS), self.get_file_data(self.result_file_path)[0])
+
+    def test_erase_lba_success(self):
+        self.sut.enable_cache()
+        for i in range(int(TEST_SIZE)):
+            self.sut.write_lba(str(int(TEST_ADDRESS)+i), str(hex(int(TEST_VALUE, 16)+i)))
+
+        self.sut.erase_lba(TEST_ADDRESS, TEST_SIZE)
+
+        for i in range(int(TEST_SIZE)):
+            self.assertEqual(self.sut.read_lba(str(int(TEST_ADDRESS)+i)), INIT_VALUE)
 
     def get_file_data(self, path):
         return pl.Path(path).read_text().split('\n')
