414e06a77c8182d00373aefcad8970ce1fb7b806
yoon-j <jiyoon.jeon258@gmail.com>
2024-07-18 10:39:22 +0900

    [fix] Fix mis-ordered logics

--- src/ssd/command_buffer.py
+++ src/ssd/command_buffer.py
@@ -114,8 +114,8 @@ class CommandBuffer:
 
         if len(self.commands_to_return) > len(temp_command_list):
             self.commands_to_return = temp_command_list
-        self.update_file()
         self.buffer_data = INIT_BUFFER_DATA
+        self.update_file()
 
     def get_commands_requiring_save(self):
         return self.commands_to_return
